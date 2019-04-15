a2cc1841ba3ca42134880f773ae131d361003038
Alexander <alexveligurskiy@gmail.com>
2019-04-15 22:16:01 +0300

    intel idea stuff

--- src/Start.java
+++ src/Start.java
@@ -3,7 +3,7 @@
 public class Start 
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
 			MainPage loginFrame = new MainPage();
 			loginFrame.setVisible(true);
