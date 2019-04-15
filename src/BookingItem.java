975dd970d600c7d178fd7b3453fabe063681d88d
Selteth <selteth@gmail.com>
2019-04-16 02:47:01 +0300

    Add booking class and ability to get all the bookings from the database

---
+++ src/BookingItem.java
@@ -0,0 +1,78 @@
+import java.util.Date;
+
+public class BookingItem {
+
+	private int id;
+	private int clientId;
+	private int roomId;
+	private Date bookDate;
+	private Date checkInDate;
+	private int duration;
+	private Date cancelDate;
+	private String cancelReason;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(int clientId) {
+		this.clientId = clientId;
+	}
+
+	public int getRoomId() {
+		return roomId;
+	}
+
+	public void setRoomId(int roomId) {
+		this.roomId = roomId;
+	}
+
+	public Date getBookDate() {
+		return bookDate;
+	}
+
+	public void setBookDate(Date bookDate) {
+		this.bookDate = bookDate;
+	}
+
+	public Date getCheckInDate() {
+		return checkInDate;
+	}
+
+	public void setCheckInDate(Date checkInDate) {
+		this.checkInDate = checkInDate;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public Date getCancelDate() {
+		return cancelDate;
+	}
+
+	public void setCancelDate(Date cancelDate) {
+		this.cancelDate = cancelDate;
+	}
+
+	public String getCancelReason() {
+		return cancelReason;
+	}
+
+	public void setCancelReason(String cancelReason) {
+		this.cancelReason = cancelReason;
+	}
+
+}
--- src/BookingLogic.java
+++ src/BookingLogic.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+import java.util.List;
 
 import database.DbUtils;
 
@@ -8,21 +9,29 @@ public class BookingLogic {
 		
 	}
 	
-	public int addBooking(String clientPhone, int roomNumber, 
+	List<BookingItem> getAll() {
+		String query = "select id_booking as id, id_client as clientId, id_room as roomId, "
+				+ "book_date as bookDate, check_in_date as checkInDate, duration as duration, "
+				+ "cancel_date as cancelDate, cancel_reason as cancelReason from booking";
+		List<BookingItem> bookings = DbUtils.GetMapped(query, BookingItem.class);
+		
+		return bookings;
+	}
+	
+	public int addBooking(int clientId, int roomId, 
 			Date bookDate, Date checkInDate, int duration) {
 		String query = "insert into `booking` (`id_client`, `id_room`, `book_date`, `check_in_date`, `duration`) values ("
-				+ "(" + getClientIdQuery(clientPhone) + "), " + "(" + getRoomIdQuery(roomNumber) + "), " 
-				+ "'" + getDateString(bookDate) + "', " + "'" + getDateString(checkInDate) 
+				+ clientId + ", " + roomId + ", " + "'" + getDateString(bookDate) + "', " + "'" + getDateString(checkInDate) 
 				+ "', " + duration + ")";
 		int insertedId = DbUtils.insertAndGetId(query);
 		
 		return insertedId;
 	}
 	
-	public void updateBooking(int bookingId, String clientPhone, int roomNumber, Date bookDate, 
+	public void updateBooking(int bookingId, int clientId, int roomId, Date bookDate, 
 			Date checkInDate, int duration, Date cancelDate, String cancelReason) {
-		String query = "update `booking` set `id_client` = " + "(" + getClientIdQuery(clientPhone) + ")"
-				+ ", `id_room` = " + "(" + getRoomIdQuery(roomNumber) + ")" + ", book_date = '" 
+		String query = "update `booking` set `id_client` = " + clientId
+				+ ", `id_room` = " + roomId + ", book_date = '" 
 				+ getDateString(bookDate) + "'" + ", check_in_date = '" + getDateString(checkInDate) + "'"
 				+ ", duration = " + duration + ", cancel_date = '" + getDateString(cancelDate) + "'"
 				+ ", cancel_reason = '" + cancelReason + "'" + " where id_booking = " + bookingId;
@@ -36,14 +45,6 @@ public class BookingLogic {
 	
 	private String getDateString(Date date) {
 		return new java.sql.Date(date.getTime()).toString() + ' ' + new java.sql.Time(date.getTime());
-	}
-	
-	private String getClientIdQuery(String clientPhone) {
-		return "select `id_client` from `client` where `phone` = '" + clientPhone + "'";
-	}
-	
-	private String getRoomIdQuery(int roomNumber) {
-		return "select `id_room` from `room` where `room_number` = " + roomNumber;
 	}
 	
 }
