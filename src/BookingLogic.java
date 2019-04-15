46d6587cac8e282c5a4ebef9c092db0ca95a719a
Selteth <selteth@gmail.com>
2019-04-16 02:03:27 +0300

    Change booking db queries

--- src/BookingLogic.java
+++ src/BookingLogic.java
@@ -8,21 +8,22 @@ public class BookingLogic {
 		
 	}
 	
-	public int addBooking(int clientId, int roomId, 
+	public int addBooking(String clientPhone, int roomNumber, 
 			Date bookDate, Date checkInDate, int duration) {
 		String query = "insert into `booking` (`id_client`, `id_room`, `book_date`, `check_in_date`, `duration`) values ("
-				+ clientId + ", " + roomId + ", " + "'" + getDateString(bookDate) + "', " + "'" + getDateString(checkInDate) 
+				+ "(" + getClientIdQuery(clientPhone) + "), " + "(" + getRoomIdQuery(roomNumber) + "), " 
+				+ "'" + getDateString(bookDate) + "', " + "'" + getDateString(checkInDate) 
 				+ "', " + duration + ")";
 		int insertedId = DbUtils.insertAndGetId(query);
 		
 		return insertedId;
 	}
 	
-	public void updateBooking(int bookingId, int clientId, int roomId, Date bookDate, 
+	public void updateBooking(int bookingId, String clientPhone, int roomNumber, Date bookDate, 
 			Date checkInDate, int duration, Date cancelDate, String cancelReason) {
-		String query = "update `booking` set `id_client` = " + clientId + ", `id_room` = " + roomId
-				+ ", book_date = '" + getDateString(bookDate) + "'" 
-				+ ", check_in_date = '" + getDateString(checkInDate) + "'"
+		String query = "update `booking` set `id_client` = " + "(" + getClientIdQuery(clientPhone) + ")"
+				+ ", `id_room` = " + "(" + getRoomIdQuery(roomNumber) + ")" + ", book_date = '" 
+				+ getDateString(bookDate) + "'" + ", check_in_date = '" + getDateString(checkInDate) + "'"
 				+ ", duration = " + duration + ", cancel_date = '" + getDateString(cancelDate) + "'"
 				+ ", cancel_reason = '" + cancelReason + "'" + " where id_booking = " + bookingId;
 		DbUtils.ExecuteQuery(query);
@@ -35,6 +36,14 @@ public class BookingLogic {
 	
 	private String getDateString(Date date) {
 		return new java.sql.Date(date.getTime()).toString() + ' ' + new java.sql.Time(date.getTime());
+	}
+	
+	private String getClientIdQuery(String clientPhone) {
+		return "select `id_client` from `client` where `phone` = '" + clientPhone + "'";
+	}
+	
+	private String getRoomIdQuery(int roomNumber) {
+		return "select `id_room` from `room` where `room_number` = " + roomNumber;
 	}
 	
 }
